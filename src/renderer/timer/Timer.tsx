30f9a1cf9e090794db1d03198707e0a0d208535a
Lukáš Peťko <lukaspetko@Lukes-Macbook-Pro-14.local>
2023-12-02 12:58:58 +0100

    adding fonts

--- src/renderer/timer/Timer.tsx
+++ src/renderer/timer/Timer.tsx
@@ -1,24 +1,31 @@
-import CircularProgress from '@/components/custom/CircularProgress';
 import { Progress } from '@/components/ui/progress';
 import secondsToTime from '@/lib/convertSeconds';
 import React, { useEffect, useState } from 'react';
 
 const Timer = () => {
-  const [totalSeconds, setTotalSeconds] = useState(25 * 60);
+  const [totalSeconds, setTotalSeconds] = useState(0.5 * 60);
   const [seconds, setSeconds] = useState(totalSeconds);
 
   useEffect(() => {
-    if (seconds <= 0) setSeconds(25 * 60);
+    if (seconds <= 0) {
+      setSeconds(totalSeconds === 0.5 * 60 ? 0.25 * 60 : 0.5 * 60);
+      setTotalSeconds(totalSeconds === 0.5 * 60 ? 0.25 * 60 : 0.5 * 60);
+    }
+
     const timer = setInterval(() => {
       setSeconds(seconds - 1);
     }, 1000);
 
     return () => clearInterval(timer);
-  }, [seconds]);
+  }, [seconds, totalSeconds]);
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen">
-      <div className="flex gap-3 flex-col items-center">
+    <div className="flex flex-col items-center justify-between w-full h-screen py-5">
+      <h1 className="text-5xl font-bold flex justify-center gap-2">
+        <span className="font-japanese text-red-700">トマト</span>
+        <span className="font-heading"> Timer</span>
+      </h1>
+      <div className="flex gap-6 flex-col items-center">
         <p className="text-4xl font-bold">{secondsToTime(seconds)}</p>
         <Progress
           value={(seconds / totalSeconds) * 100}
@@ -26,6 +33,7 @@ const Timer = () => {
           indicatorClassName="bg-red-700"
         />
       </div>
+      <div />
     </div>
   );
 };
